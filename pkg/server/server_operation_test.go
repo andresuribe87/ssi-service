12b55283eedafe4a5fcc0117e3a30647e1ebd949
Andres Uribe Gonzalez <auribe@squareup.com>
2022-12-21 17:08:41 -0500

    Add a test for GetOperation after the applications.

--- pkg/server/server_operation_test.go
+++ pkg/server/server_operation_test.go
@@ -8,11 +8,15 @@ import (
 	"testing"
 
 	"github.com/TBD54566975/ssi-sdk/credential/exchange"
+	"github.com/TBD54566975/ssi-sdk/crypto"
 	"github.com/goccy/go-json"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/mr-tron/base58"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	credmodel "github.com/tbd54566975/ssi-service/internal/credential"
+	"github.com/tbd54566975/ssi-service/internal/keyaccess"
 	"github.com/tbd54566975/ssi-service/pkg/server/router"
 	"github.com/tbd54566975/ssi-service/pkg/service/operation"
 	"github.com/tbd54566975/ssi-service/pkg/service/operation/submission"
@@ -52,6 +56,51 @@ func TestOperationsAPI(t *testing.T) {
 	})
 
 	t.Run("GetOperation", func(t *testing.T) {
+		t.Run("Returns operation after application", func(tt *testing.T) {
+			s := setupTestDB(tt)
+			svc, err := operation.NewOperationService(s)
+			assert.NoError(tt, err)
+			opRouter, err := router.NewOperationRouter(svc)
+			assert.NoError(tt, err)
+			keyStoreService := testKeyStoreService(t, s)
+			didService := testDIDService(t, s, keyStoreService)
+			didRouter, err := router.NewDIDRouter(didService)
+			assert.NoError(tt, err)
+			schemaService := testSchemaService(t, s, keyStoreService, didService)
+			credentialService := testCredentialService(t, s, keyStoreService, didService, schemaService)
+			mRouter, _ := testManifest(t, s, keyStoreService, didService, credentialService)
+			didSvc := testDIDService(t, s, keyStoreService)
+			schemaSvc := testSchemaService(t, s, keyStoreService, didSvc)
+			schemaRouter, err := router.NewSchemaRouter(schemaSvc)
+			assert.NoError(tt, err)
+			credSvc := testCredentialService(t, s, keyStoreService, didSvc, schemaSvc)
+			credRouter, err := router.NewCredentialRouter(credSvc)
+			assert.NoError(tt, err)
+
+			issuerDID := createDID(t, didRouter)
+			subjectDID := createDID(t, didRouter)
+			schemaResponse := createSchema(t, schemaRouter, issuerDID.DID.ID)
+			credData := map[string]any{"licenseType": "WA-DL-CLASS-A"}
+			cred := createCredential(t, credRouter, issuerDID.DID.ID, subjectDID.DID.ID, schemaResponse.ID, credData)
+			manifest := createManifest(t, mRouter, issuerDID.DID.ID, schemaResponse.ID)
+			applicationOp := submitApplication(t, mRouter, manifest, cred.CredentialJWT, subjectDID)
+
+			createdID := applicationOp.ID
+			req := httptest.NewRequest(
+				http.MethodPut,
+				fmt.Sprintf("https://ssi-service.com/v1/operations/%s", createdID),
+				nil)
+			w := httptest.NewRecorder()
+
+			err = opRouter.GetOperation(newRequestContextWithParams(map[string]string{"id": createdID}), w, req)
+
+			assert.NoError(tt, err)
+			var resp router.Operation
+			assert.NoError(tt, json.NewDecoder(w.Body).Decode(&resp))
+			assert.True(tt, resp.Done)
+			assert.Contains(tt, resp.ID, "credentials/responses/")
+		})
+
 		t.Run("Returns operation after submission", func(tt *testing.T) {
 			s := setupTestDB(tt)
 			pRouter := setupPresentationRouter(t, s)
@@ -272,6 +321,129 @@ func TestOperationsAPI(t *testing.T) {
 		})
 	})
 
+}
+
+func createCredential(tt *testing.T, credRouter *router.CredentialRouter, issuerDID, subjectDID, schemaID string, data map[string]any) router.CreateCredentialResponse {
+	request := router.CreateCredentialRequest{
+		Issuer:  issuerDID,
+		Subject: subjectDID,
+		Schema:  schemaID,
+		Data:    data,
+	}
+
+	value := newRequestValue(tt, request)
+	req := httptest.NewRequest(
+		http.MethodPut,
+		"https://ssi-service.com/v1/credentials",
+		value)
+	w := httptest.NewRecorder()
+
+	err := credRouter.CreateCredential(newRequestContext(), w, req)
+
+	assert.NoError(tt, err)
+
+	var resp router.CreateCredentialResponse
+	assert.NoError(tt, json.NewDecoder(w.Body).Decode(&resp))
+	return resp
+}
+
+func submitApplication(tt *testing.T, mRouter *router.ManifestRouter, manifest router.CreateManifestResponse, credJWT *keyaccess.JWT, applicantDID router.CreateDIDByMethodResponse) router.Operation {
+	m := manifest.Manifest
+
+	container := []credmodel.Container{{CredentialJWT: credJWT}}
+
+	applicationRequest := getValidApplicationRequest(m.ID, m.PresentationDefinition.ID, m.PresentationDefinition.InputDescriptors[0].ID, container)
+
+	applicantPrivKeyBytes, err := base58.Decode(applicantDID.PrivateKeyBase58)
+	assert.NoError(tt, err)
+	applicantPrivKey, err := crypto.BytesToPrivKey(applicantPrivKeyBytes, applicantDID.KeyType)
+	assert.NoError(tt, err)
+	signer, err := keyaccess.NewJWKKeyAccess(applicantDID.DID.ID, applicantPrivKey)
+	assert.NoError(tt, err)
+	signed, err := signer.SignJSON(applicationRequest)
+	assert.NoError(tt, err)
+
+	request := router.SubmitApplicationRequest{
+		ApplicationJWT: *signed,
+	}
+
+	value := newRequestValue(tt, request)
+	req := httptest.NewRequest(
+		http.MethodPut,
+		"https://ssi-service.com/v1/dids/key",
+		value)
+	w := httptest.NewRecorder()
+
+	err = mRouter.SubmitApplication(newRequestContext(), w, req)
+
+	assert.NoError(tt, err)
+
+	var resp router.Operation
+	assert.NoError(tt, json.NewDecoder(w.Body).Decode(&resp))
+	return resp
+}
+
+func createSchema(tt *testing.T, schemaRouter *router.SchemaRouter, author string) router.CreateSchemaResponse {
+	licenseSchema := map[string]any{
+		"type": "object",
+		"properties": map[string]any{
+			"licenseType": map[string]any{
+				"type": "string",
+			},
+		},
+		"additionalProperties": true,
+	}
+	request := router.CreateSchemaRequest{Author: author, Name: "license schema", Schema: licenseSchema, Sign: true}
+	value := newRequestValue(tt, request)
+	req := httptest.NewRequest(
+		http.MethodPut,
+		"https://ssi-service.com/v1/schemas",
+		value)
+	w := httptest.NewRecorder()
+
+	err := schemaRouter.CreateSchema(newRequestContext(), w, req)
+
+	assert.NoError(tt, err)
+
+	var resp router.CreateSchemaResponse
+	assert.NoError(tt, json.NewDecoder(w.Body).Decode(&resp))
+	return resp
+}
+
+func createDID(tt *testing.T, didRouter *router.DIDRouter) router.CreateDIDByMethodResponse {
+	request := router.CreateDIDByMethodRequest{
+		KeyType: crypto.Ed25519,
+	}
+	value := newRequestValue(tt, request)
+	req := httptest.NewRequest(
+		http.MethodPut,
+		"https://ssi-service.com/v1/dids/key",
+		value)
+	w := httptest.NewRecorder()
+
+	err := didRouter.CreateDIDByMethod(newRequestContextWithParams(map[string]string{"method": "key"}), w, req)
+
+	assert.NoError(tt, err)
+
+	var resp router.CreateDIDByMethodResponse
+	assert.NoError(tt, json.NewDecoder(w.Body).Decode(&resp))
+	return resp
+
+}
+func createManifest(tt *testing.T, manifestRouter *router.ManifestRouter, issuerDID, schemaID string) router.CreateManifestResponse {
+	createManifestRequest := getValidManifestRequest(issuerDID, schemaID)
+
+	requestValue := newRequestValue(tt, createManifestRequest)
+	req := httptest.NewRequest(http.MethodPut, "https://ssi-service.com/v1/manifests", requestValue)
+	w := httptest.NewRecorder()
+	err := manifestRouter.CreateManifest(newRequestContext(), w, req)
+	assert.NoError(tt, err)
+
+	var resp router.CreateManifestResponse
+	err = json.NewDecoder(w.Body).Decode(&resp)
+	assert.NoError(tt, err)
+
+	return resp
 }
 
 func setupTestDB(t *testing.T) storage.ServiceStorage {
